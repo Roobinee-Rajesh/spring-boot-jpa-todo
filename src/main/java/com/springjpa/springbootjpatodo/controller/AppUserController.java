708e4e424374181758f7e862233946f7d75e48e5
Roobinee-Rajesh <roobinee.rajesh@kumaran.com>
2023-11-13 19:03:17 +0530

    commit

--- src/main/java/com/springjpa/springbootjpatodo/controller/AppUserController.java
+++ src/main/java/com/springjpa/springbootjpatodo/controller/AppUserController.java
@@ -4,11 +4,9 @@ import com.springjpa.springbootjpatodo.model.AppUser;
 import com.springjpa.springbootjpatodo.service.AppUserService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/appuser")
 public class AppUserController {
